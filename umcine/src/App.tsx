93ac1ae210a0b6c2b8f97daf74a9ffe4efe313ce
tjdals-dbs <andyoon502@naver.com>
2026-09-27 00:21:56 +0900

    practice: 미니 실습: 영화 카드 재사용하기

--- umcine/src/App.tsx
+++ umcine/src/App.tsx
@@ -1,30 +1,41 @@
-function Header() {
-  return <h1>영화 목록</h1>;
+interface MovieCardProps {
+  title: string;
+  releaseDate: string;
+  isBookmarked: boolean;
 }
 
-function MovieCard() {
+function MovieCard({
+  title,
+  releaseDate,
+  isBookmarked,
+}: MovieCardProps) {
   return (
     <article>
-      <h2>오디세이</h2>
-      <p>2026.08.05</p>
+      <h2>{title}</h2>
+      <p>{releaseDate}</p>
+      <p>{isBookmarked ? "북마크됨" : "북마크 안 됨"}</p>
     </article>
-  );
-}
-
-function MovieList() {
-  return(
-    <section>
-      <MovieCard />
-      <MovieCard />
-    </section>
   );
 }
 
 export default function App() {
   return (
     <main>
-      <Header />
-      <MovieList />
+      <MovieCard
+        title="오디세이"
+        releaseDate="2026.08.05"
+        isBookmarked={true}
+      />
+      <MovieCard
+        title="토이 스토리 5"
+        releaseDate="2026.06.17"
+        isBookmarked={false}
+      />
+      <MovieCard
+        title="스파이더맨 브랜드 뉴 데이"
+        releaseDate="2026.07.29"
+        isBookmarked={true}
+      />
     </main>
   );
 }
